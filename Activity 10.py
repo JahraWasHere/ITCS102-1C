f2bf568e21addf42a9b20483822e11d9b899a832
Jahra <fabonjahra4@gmail.com>
2025-08-27 13:56:18 +0800

    Activity 10.py

---
+++ Activity 10.py
@@ -0,0 +1,12 @@
+#from getpass import getpass
+
+username = 'JahraWasHere'
+password = 'test'
+
+u = input("Enter your Username here ---> ")
+p = input("Enter your Password here ---> ")
+
+if (u == username) and (p == password) :
+	print("Welcome User!")
+else:
+	print("You're not welcome here, Get out")
