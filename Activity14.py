4beb5054b46c5f211a976adf3061aebf4ec1c235
Jahra <fabonjahra4@gmail.com>
2025-09-29 14:27:37 +0800

    Activity14.py

---
+++ Activity14.py
@@ -0,0 +1,23 @@
+#Goal: Print 20 to 1
+
+#IMPORTANT: REMOVE THE HASHTAG ON EITHER CODE TO TEST WHICH ONE YOU PREFER!
+
+#The Code Below is an Ascending Loop
+#             |
+#             |
+#             |
+#             |
+#             V
+
+#for loop in range(1,21,1):
+#    print(loop)
+
+#The Code Below is a Descending Loop
+#             |
+#             |
+#             |
+#             |
+#             V
+
+#for loop in range(20,0,-1):
+#    print(loop)
