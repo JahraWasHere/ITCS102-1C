d4b002279ae23d3e17e11fe4a857dbeeb5aa27be
Jahra <fabonjahra4@gmail.com>
2025-09-29 14:29:43 +0800

    Activity19.py

---
+++ Activity19.py
@@ -0,0 +1,21 @@
+#Goal: Create a Diamond
+
+print("\t\t   *", end=" ")
+for loop in range(1,11,1):
+    for x in range(11,loop,-1):
+        print(" ", end= " ")
+    for y in range(1,loop,1):
+        print("*", end= " ")
+    for z in range(1,loop,1):
+        print("*", end= " ")
+    print()
+
+for loop in range(1,11,1):
+    for x in range(1,loop,1):
+        print(" ", end= " ")
+    for y in range(11,loop,-1):
+        print("*", end= " ")
+    for z in range(11,loop,-1):
+        print("*", end= " ")
+    print()
+print("\t\t   *", end=" ")
