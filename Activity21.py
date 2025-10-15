b5cf7e549d9604f7150dda250ea7147d3310c5ea
Jahra <fabonjahra4@gmail.com>
2025-10-15 14:23:58 +0800

    Activity21.py

--- Activity21.py
+++ Activity21.py
@@ -1,13 +1,13 @@
 import random
 
 print("********************\n\nWelcome to the Gambling Game!\n\n********************")
-print("How Do You Play? Simple, Guess The number I'm Thinking of from 1-10!")
+print("How Do You Play? Simple! Guess The number I'm Thinking of from 1-10!")
 
 random_value = random.randint(1,10)
 tries = 0
 variable = True
 
-name = input("Now Enter Your Name, My Dear Guest! ---> ")
+name = input("But First! Let Me Know Your Name, My Dear Guest! ---> ")
 
 while variable == True:
     num = eval(input("Now Enter the Number You're Going to Guess! ---> "))
