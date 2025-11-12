1d03363b6610763790248448bc9eca38a49da41c
Jahra <fabonjahra4@gmail.com>
2025-11-12 14:03:55 +0800

    Activity23.py

--- Activity23.py
+++ Activity23.py
@@ -1,4 +1,4 @@
-from Activity23_1 import greet, greetings, Return
+from Activity23_1 import greet, greetings, Return, Factorial
 
 def custom():
     print("This is a custom function!\nWhat it does is it prints this sentence out when called")
@@ -9,3 +9,4 @@ greetings("Jahra", "Lucena", "18")
 
 print(f"I want to get the summation of {Return(8)}")
 print(f"I want to get the summation of {Return(10)}")
+print(f"I want to get the Factorial of {Factorial(6)}")
