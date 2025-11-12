9ac7c1355d9d7aa795f27833dcdd46869f6dd9cd
Jahra <fabonjahra4@gmail.com>
2025-11-12 13:34:37 +0800

    Activity23.py

--- Activity23.py
+++ Activity23.py
@@ -1,10 +1,7 @@
-#Goal: Create a Custom Function using def
-#MAKE SURE THAT THE NAME OF THE FUNCTION DOES NOT HAVE A SPACE
+from Activity23_1 import greet
 
 def custom():
     print("This is a custom function!\nWhat it does is it prints this sentence out when called")
 
 custom()
-custom()
-custom()
-custom()
+greet("Jahra")
