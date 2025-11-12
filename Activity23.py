b10ec58f504dd2d1fda42473df576a587eedf29f
Jahra <fabonjahra4@gmail.com>
2025-11-12 13:23:51 +0800

    Activity23.py

---
+++ Activity23.py
@@ -0,0 +1,10 @@
+#Goal: Create a Custom Function using def
+#MAKE SURE THAT THE NAME OF THE FUNCTION DOES NOT HAVE A SPACE
+
+def custom():
+    print("This is a custom function!\nWhat it does is it prints this sentence out when called")
+
+custom()
+custom()
+custom()
+custom()
