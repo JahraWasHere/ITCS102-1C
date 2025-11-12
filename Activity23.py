ebba79c777ab3de07e93fd456fecbab8340ed0b3
Jahra <fabonjahra4@gmail.com>
2025-11-12 13:56:42 +0800

    Activity23.py

--- Activity23.py
+++ Activity23.py
@@ -1,7 +1,11 @@
-from Activity23_1 import greet
+from Activity23_1 import greet, greetings, Return
 
 def custom():
     print("This is a custom function!\nWhat it does is it prints this sentence out when called")
 
 custom()
 greet("Jahra")
+greetings("Jahra", "Lucena", "18")
+
+print(f"I want to get the summation of {Return(8)}")
+print(f"I want to get the summation of {Return(10)}")
