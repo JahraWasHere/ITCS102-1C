2c3b4c6a45302a27110ae7b3d84383fe1869e373
Jahra <fabonjahra4@gmail.com>
2025-11-12 13:55:53 +0800

    Activity23_1.py

--- Activity23_1.py
+++ Activity23_1.py
@@ -1,2 +1,11 @@
 def greet(name):
     print(f"Hello {name}! Nice to meet you!")
+
+def greetings(name, loc, age):
+    print(f"Hello {name}! From {loc}, Aged {age}")
+
+def Return(number):
+    sum = 0
+    for x in range(1, number+1, 1):
+        sum += x
+    return sum
