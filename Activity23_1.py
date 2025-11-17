3b54ef46bd4e2679274a8ddd6489e8c8ee78f789
Jahra <fabonjahra4@gmail.com>
2025-11-17 14:32:43 +0800

    Activity23_1.py

--- Activity23_1.py
+++ Activity23_1.py
@@ -15,3 +15,14 @@ def Factorial(number):
     for x in range(number,0,-1):
         factorial *= x
     return factorial
+
+def triangle(size):
+    for x in range(1,size,1):
+        for a in range(size,x,-1):
+            print(" ",end=" ")
+        for b in range(x,0,-1):
+            print("*",end=" ")
+        for c in range(x,0+1,-1):
+            print("*",end=" ")
+        print()
+    return
