4e380eaa4e6f9617660e66afb982a017a9b5223f
Jahra <fabonjahra4@gmail.com>
2025-11-17 14:33:08 +0800

    Activity25.py

---
+++ Activity25.py
@@ -0,0 +1,9 @@
+#LIST
+
+Sentinels_In_Forsaken = ["Shedletsky", "Two Time", "Guest 1337", "Chance"]
+print(Sentinels_In_Forsaken[0])
+
+#DICTIONARY
+
+Sentinels_In_Forsaken = {"Best Sentinel" : "Shedletsky", "Good Sentinel" : "Two Time", "Worst Sentinel" : "Guest 1337", "Great Sentinel" : "Chance"}
+print(Sentinels_In_Forsaken["Worst Sentinel"])
